888dd28ab7291e853bef0927d3b99cc451ed107b
Kavindu Akash <bkkasamarasingha1220@gmail.com>
2023-12-30 10:00:38 +0530

    dev first route

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,21 @@ import express from 'express';
 // invoke the express
 const app = express();
 
+
+app.get('/user/all', (req: express.Request, res: express.Response) => {
+
+    let data = {
+        _id: "asdasdasdasdasda",
+        username: "pathums",
+        fname: "Pathum",
+        lname: "Silva",
+        email: "pathums@ijse.lk"
+    }
+
+    res.send(data);
+})
+
+
 // start the server
 app.listen(8081, () => {
     console.log("Server started on port 8081")
