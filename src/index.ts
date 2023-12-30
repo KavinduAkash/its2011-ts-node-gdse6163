44f5a83afbe8194416287c4e50cbc36c5eccabb4
Kavindu Akash <bkkasamarasingha1220@gmail.com>
2023-12-30 09:40:31 +0530

    apply express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("hello!");
+// const express = require('express');
+import express from 'express';
+
+// invoke the express
+const app = express();
+
+// start the server
+app.listen(8081, () => {
+    console.log("Server started on port 8081")
+})
