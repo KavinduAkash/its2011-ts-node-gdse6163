577d876a078938f774581e2ced31fe90f06b2463
Kavindu Akash <bkkasamarasingha1220@gmail.com>
2023-12-31 20:43:18 +0530

    dev update article

--- src/index.ts
+++ src/index.ts
@@ -267,6 +267,46 @@ app.get('/article/get/my', verifyToken, async (req: express.Request, res: any) =
     }
 })
 
+app.put('/article', verifyToken, async (req: express.Request, res: any) => {
+    try {
+
+        let req_body: any = req.body
+
+        let user_id = res.tokenData.user._id;
+
+        let article = await ArticleModel.find({_id: req_body.id ,user: user_id})
+
+        console.log('test');
+
+        if(article) {
+
+            await ArticleModel.findOneAndUpdate({_id: req_body.id}, {
+                title: req_body.title,
+                description: req_body.description
+            })
+            .then(r => {
+                res.status(200).send(
+                    new CustomResponse(100, "Article updated successfully.")
+                )
+            }).catch(error => {
+                    console.log(error)
+                    res.status(100).send(
+                        new CustomResponse(100, "Something went wrong.")
+                    )
+            })
+
+        } else {
+            res.stat(401).send(
+                new CustomResponse(401, "Access denied")
+            )
+        }
+
+
+    } catch (error) {
+        res.status(100).send("Error");
+    }
+})
+
 // start the server
 app.listen(8081, () => {
     console.log("Server started on port 8081")
